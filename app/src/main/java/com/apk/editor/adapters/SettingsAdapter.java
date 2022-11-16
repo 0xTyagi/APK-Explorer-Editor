8e9367b33d76dd17e3b1a035ebb2a5b3c73148b5
Nik-Kot <42574700+Nik-Kot@users.noreply.github.com>
2022-11-16 22:46:43 +0300

    Fix Settings Adapter displaying settings inconsistently (caused by not updating values reused by RecyclerView)

--- app/src/main/java/com/apk/editor/adapters/SettingsAdapter.java
+++ app/src/main/java/com/apk/editor/adapters/SettingsAdapter.java
@@ -31,8 +31,6 @@ public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHo
 
     private static ArrayList<sSerializableItems> data;
 
-    int dd = 0;
-
     public SettingsAdapter(ArrayList<sSerializableItems> data) {
         SettingsAdapter.data = data;
     }
@@ -42,60 +40,50 @@ public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.ViewHo
     public SettingsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View rowItem = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_view_about, parent, false);
         Log.e("qqq", viewType+ " ");
-        ViewHolder holder = new ViewHolder(rowItem);
-        holder.id = dd;
-        dd++;
-        return holder;
+        return new ViewHolder(rowItem);
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public void onBindViewHolder(@NonNull SettingsAdapter.ViewHolder holder, int position) {
-        Log.e("qqq", position + " " + holder.id);
         holder.Title.setText(data.get(position).getTextOne());
-        Log.e("qqq", data.get(position).getTextOne());
         if (data.get(position).getTextTwo() != null) {
-            holder.mIcon.setVisibility(View.VISIBLE);
+            holder.Description.setVisibility(View.VISIBLE);
             holder.Description.setText(data.get(position).getTextTwo());
-            Log.e("qqq", data.get(position).getTextTwo());
         } else {
             holder.Description.setVisibility(View.GONE);
-            Log.e("qqq", "Text2 is null");
         }
         if (sUtils.isDarkTheme(holder.Title.getContext())) {
             holder.Title.setTextColor(APKEditorUtils.getThemeAccentColor(holder.Title.getContext()));
         }
         if (position != 9 && position != 10) {
             holder.mIcon.setColorFilter(sUtils.isDarkTheme(holder.Title.getContext()) ? Color.WHITE : Color.BLACK);
-            Log.e("qqq", "filling");
         } else {
             holder.mIcon.clearColorFilter();
         }
         if (data.get(position).getIcon() != null) {
             holder.mIcon.setVisibility(View.VISIBLE);
             holder.mIcon.setImageDrawable(data.get(position).getIcon());
-            Log.e("qqq", data.get(position).getIcon().toString());
         } else {
             holder.mIcon.setVisibility(View.GONE);
-            Log.e("qqq", "Icon is null");
         }
         if (Build.VERSION.SDK_INT >= 29 && (position == 5 || position == 6)) {
             holder.Title.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
             holder.Description.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
-            Log.e("qqq", "Strike through");
+        } else {
+            holder.Title.setPaintFlags(Paint.HINTING_OFF);
+            holder.Description.setPaintFlags(Paint.HINTING_OFF);
         }
     }
 
     @Override
     public int getItemCount() {
-        //Log.e("qqq", "size: " + data.size());
         return data.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final AppCompatImageButton mIcon;
         private final MaterialTextView Description, Title;
-        public int id;
 
         public ViewHolder(View view) {
             super(view);
